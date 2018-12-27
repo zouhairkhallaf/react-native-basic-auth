e2f530b47d3e1c66f7939e6c0b133f6f22d2b92e
Zouhair Khallaf <zouhairkhallaf@gmail.com>
2018-12-27 17:11:07 -0500

    Added auth project to my GH

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+
+
+class App extends Component {
+    render(){
+        return (
+            <View>
+                <Text>
+                    Hi there !
+                </Text>
+            </View>
+        );
+    };
+};
+
+export default App;
